bc2cfe27f5906d596e285d79696893bfdcbd1e27
fuston05 <fuston05@yahoo.com>
2020-02-14 12:50:54 -0500

    wrote all the test i could without having learned to test axios call.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {render} from '@testing-library/react'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test('is h1, button being displayed?', () => {
+  const {getByText}= render( <App /> );
+
+  getByText(/player information/i);
+  getByText(/dark mode/i);
+
+})
+
+
