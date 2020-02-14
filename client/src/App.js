ab372990903cbd1824843f6cf273b4619763e522
fuston05 <fuston05@yahoo.com>
2020-02-14 11:27:46 -0500

    got components created, got data to display in cards

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+//components
+import Players from './components/Players/Players';
+
+//styles
 import './App.css';
 
 function App() {
+  // state
+  const [data, setData]= useState([]);
+  //functions
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:5000/api/players')
+    .then(res => {
+      console.log(res.data);
+      setData(res.data)
+    })
+    .catch(err => {console.log(err);})
+  }, [])
+
   return (
     <div className="App">
       <h1> test</h1>
+      <Players data= {data} />
     </div>
   );
 }
